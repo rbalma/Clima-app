9c8dc82b1b750f8ab17a1ca0cec03b4186a8ca05
rbalma <balmarodrigo45@gmail.com>
2021-04-14 17:30:50 -0300

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+
+   //state del formulario
+   const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    if(consultar){
+        const consultarAPI = async() => {
+        const appId = '22aad2746a86337a0b8029db57950cf4';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setResultado(resultado);
+        setConsultar(false);
+
+        //Detecta si hubo resultados correctos en la consulta
+          if(resultado.cod === "404") {
+            setError(true);
+          } else {
+            setError(false);
+          }
+      }
+      consultarAPI();
+    }
+  // eslint-disable-next-line
+  }, [consultar]);
+
+  let componente;
+  if(error){
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima resultado={resultado} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header 
+        titulo='Clima React App'
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12 ">
+              <Formulario 
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12 ">
+              {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
